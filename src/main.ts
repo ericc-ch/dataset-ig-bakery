761801dd4c2ae92930bea7d2d646c3da26148ac1
Erick Christian <erickchristian48@gmail.com>
2026-05-01 16:20:05 +0700

    playwright

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-export const one = 1
-export const two = 2
+import { chromium } from "playwright"
+import which from "which"
 
-export function add(a: number, b: number): number {
-  return a + b
-}
+const profilePath = new URL("../.browser-profile/", import.meta.url)
+const executablePath = await which("helium")
+
+const context = await chromium.launchPersistentContext(profilePath.pathname, {
+  executablePath,
+  headless: false,
+})
+
+const page = await context.newPage()
+await page.goto("https://google.com")
